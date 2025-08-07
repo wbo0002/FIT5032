5e4c46dfcae43dfdd21b2e1d25e954d0ad526238
Wichayada Booncharoen <wboo0002@student.monash.edu>
2025-08-07 23:48:51 +1000

    Initial commit

---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<template>
+  <div class="my-name">
+    <h2>Welcome to Vue,</h2>
+    <h2>Wichayada Booncharoen!</h2>
+  </div>
+</template>
+
+<style scoped>
+.my-name {
+  text-align: center;
+  margin-top: 20px;
+  font-size: 1.5rem;
+  color: #FFB6C1;
+}
+</style>
